68b991d966bdbb48948a3a4e457a44b8f46668bd
Olivier Bellone <olivier@thatch.ai>
2024-08-31 18:09:19 -0700

    Drop support for EOL versions of Ruby and Rails

--- test/orm/active_record.rb
+++ test/orm/active_record.rb
@@ -5,11 +5,7 @@ require 'active_record'
 ActiveRecord::Migration.verbose = false
 ActiveRecord::Base.logger = Logger.new(nil)
 
-if Rails.gem_version >= Gem::Version.new('6.0.0')
-  ActiveRecord::MigrationContext.new(File.expand_path('../dummy/db/migrate', __dir__), ActiveRecord::SchemaMigration).migrate
-elsif Rails.gem_version >= Gem::Version.new('5.2.0')
-  ActiveRecord::MigrationContext.new(File.expand_path('../dummy/db/migrate', __dir__)).migrate
-end
+ActiveRecord::MigrationContext.new(File.expand_path('../dummy/db/migrate', __dir__), ActiveRecord::SchemaMigration).migrate
 
 DatabaseCleaner[:active_record].strategy = :transaction
 ORMInvalidRecordException = ActiveRecord::RecordInvalid
